aff3689fcdf6c41c85781c134de094c2d9118c01
mihsouza2906 <mih_jovemaprendiz@hotmail.com>
2019-02-19 22:16:15 -0300

    Ola mundo aula 1

---
+++ index.php
@@ -0,0 +1,92 @@
+	<?php
+
+		/*Inicializando a interação com o PHP
+		Saída de dados na tela do usuário
+		comandos echo, van_dump, print_r*/ 
+
+		/*echo "Eu não gostei do easyphp";
+		print("Não instalar easyphp");
+		$vetor = array('Palio','Gol','Fiesta','Evoque');
+		var_dump($vetor);
+
+		$vetor = array('Palio','Gol','Fiesta','Evoque');
+
+		print_r($vetor);
+
+		$nome = "João";
+		$sobrenome = "da Silva";
+		echo "$nome,$sobrenome";
+
+		// dicas do Prof°
+		Nunca faça isso
+		Variaveis indicadas com números
+		não deixe espaços entre as variáveis
+		Não use caracteres especiais ! @ # & ¨ * () % 
+		Não crie variaveis com mais de 15 caracteres
+		use palavras em minusculo 
+		em variaveis compostas use a primeira letra da segunda
+		palavra em minusculo ex: codPagto
+		ou use underline (_) para separar palavras
+		ex: cod_pagto 
+
+
+		/*Variaveis booleano
+
+		$exibir_nome = True;
+		if (exibir_nome) {
+			echo "João da Silva";
+		}
+
+		$umidade = 91;
+
+		$vai_chover = ("$umidade > 90");
+
+		if ($vai_chover) {
+			# code...
+			echo "Está chovendo";
+
+		}else{
+
+			echo "Não está chovendo";
+		}
+
+		//Variavel do tipo numerico
+
+		$a = 1234; // numero decimal
+		$a = -1234; // negativo
+		$a = 0123; // numeração octagonal
+		$a = 0x1a; // hexadecimal
+		$a = 1.234; // ponto flutuante
+		$a = 4e23; // notações cientificas
+
+		// variéveis do tipo string
+
+		$variavel = 'Isto é um teste';
+		$variavel = "Isto é um teste";
+		echo "$variavel";
+
+		//Variáveis do tipo Array 
+
+		$carros = array ('Evoque','Veloster','Toro');
+		echo $carros[1];
+
+		class Computador
+		{
+			var $cpu;
+
+			function ligar()
+			{
+				echo "Ligando o computador a {$this ->cpu...";
+
+			}
+		}
+
+		$obj = new Computador;
+		$obj-> cpu="500 mhz";
+		$obj-> ligar();*/
+
+
+
+	?>	
+
+
